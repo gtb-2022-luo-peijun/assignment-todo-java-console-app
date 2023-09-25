27fbfc6b00d35369d6d9429653373942424f5836
LuoPeijun <peijun.luo@thoughtworks.com>
2023-09-25 12:23:50 +0800

    feat: add list condition

--- src/main/java/tw/cn/cap/gtb/todo/App.java
+++ src/main/java/tw/cn/cap/gtb/todo/App.java
@@ -2,7 +2,9 @@ package tw.cn.cap.gtb.todo;
 
 import com.google.common.base.Joiner;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,6 +27,11 @@ public class App {
         final var filePath = Path.of(homePath + File.separator + ".todo");
         final var fileNameWithPath = filePath + File.separator + "tasks";
 
+        if (!Files.exists(filePath) && !"init".equals(args[0])) {
+            System.out.println(String.format("Please run 'init' before running '%s' command.", args[0]));
+            return;
+        }
+
         if ("init".equals(args[0])) {
             if (!Files.exists(filePath)) {
                 Files.createDirectory(filePath);
@@ -37,9 +44,48 @@ public class App {
             return;
         }
 
+        long lineCount = 0;
+        Path fileName = Path.of(fileNameWithPath);
+        try {
+            lineCount = Files.lines(fileName).count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         if ("add".equals(args[0])) {
             final var taskName = Joiner.on(" ").join(Arrays.stream(args).skip(1).toArray(String[]::new));
-            Files.write(Paths.get(fileNameWithPath), taskName.concat("\n").getBytes(), StandardOpenOption.APPEND);
+            Files.write(fileName, String.format("- %d ", (lineCount + 1)).concat(taskName.concat(System.lineSeparator())).getBytes(), StandardOpenOption.APPEND);
+        }
+
+        if ("list".equals(args[0])) {
+            try {
+                System.out.println("# To be done");
+                String[] toBeDoneList = Files.lines(fileName)
+                        .filter(line -> line.startsWith("-"))
+                        .map(line -> line.replaceAll("- ", ""))
+                        .toArray(String[]::new);
+
+                if (toBeDoneList.length != 0) {
+                    Arrays.stream(toBeDoneList).forEach(System.out::println);
+                } else {
+                    System.out.println("Empty");
+                }
+
+                System.out.println("# Completed");
+                String[] completedList = Files.lines(fileName)
+                        .filter(line -> line.startsWith("x"))
+                        .map(line -> line.replaceAll("x ", ""))
+                        .toArray(String[]::new);
+
+                if (completedList.length != 0) {
+                    Arrays.stream(completedList).forEach(System.out::println);
+                } else {
+                    System.out.println("Empty");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
