55cc7c6e5d39c2ef539c77988c227c89ab2761f0
LuoPeijun <peijun.luo@thoughtworks.com>
2023-09-25 11:59:55 +0800

    feat: add demo

--- src/main/java/tw/cn/cap/gtb/todo/App.java
+++ src/main/java/tw/cn/cap/gtb/todo/App.java
@@ -1,11 +1,45 @@
 package tw.cn.cap.gtb.todo;
 
+import com.google.common.base.Joiner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("usage: todo <init|list|add|edit|remove|mark|unmark|reset> [args]");
+            return;
+        }
+
+        final var homePath = System.getProperty("user.home");
+        final var filePath = Path.of(homePath + File.separator + ".todo");
+        final var fileNameWithPath = filePath + File.separator + "tasks";
+
+        if ("init".equals(args[0])) {
+            if (!Files.exists(filePath)) {
+                Files.createDirectory(filePath);
+            }
+            final var file = new File(fileNameWithPath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            System.out.println("Initialized successfully.");
+            return;
+        }
+
+        if ("add".equals(args[0])) {
+            final var taskName = Joiner.on(" ").join(Arrays.stream(args).skip(1).toArray(String[]::new));
+            Files.write(Paths.get(fileNameWithPath), taskName.concat("\n").getBytes(), StandardOpenOption.APPEND);
+        }
     }
 }
